36432ff6280397a482814042cb9e8eb73e11dd8f
srinivasan.suresh@iopex.com <srinivasan.suresh@iopex.com>
2023-03-10 06:06:07 +0000

    v8

--- models/stg_lead.sql
+++ models/stg_lead.sql
@@ -1,9 +1,7 @@
-{{
-    config(
-        materialized='table'
-    )
-}}
+{{ config(
+    post_hook="{{ pm_utils.generate_id('Case_ID_internal') }}"
+) }}
 
 With Lead_table as
 (Select * from "Lead")
-Select * from Lead_table
+Select Lead_table."Case_ID_internal" from Lead_table
