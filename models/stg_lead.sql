023b5474b5f7ee367621aa37bacfd2bc50eaba88
srinivasan.suresh@iopex.com <srinivasan.suresh@iopex.com>
2023-03-10 06:16:43 +0000

    v9

--- models/stg_lead.sql
+++ models/stg_lead.sql
@@ -1,7 +1,5 @@
-{{ config(
-    post_hook="{{ pm_utils.generate_id('Case_ID_internal') }}"
-) }}
+
 
 With Lead_table as
-(Select * from "Lead")
+(Select * from HYPER_DB.PUBLIC."Lead")
 Select * from Lead_table
